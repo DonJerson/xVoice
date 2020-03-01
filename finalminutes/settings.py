f89135c4f778c367355456fadd73781e66a32231
prieto <prieto@desktop.com>
2020-03-01 14:31:18 -0400

    autoUpdate

--- finalminutes/settings.py
+++ finalminutes/settings.py
@@ -11,6 +11,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['xvoice.xyz','127.0.0.1','www.xvoice.xyz']
 
 INSTALLED_APPS = [
+    "sslserver",
     'kamailio',
     'rest_framework',
     'django.contrib.contenttypes',
@@ -99,9 +100,35 @@ USE_TZ = True
 
 USE_S3 = os.getenv('USE_S3') == 'TRUE'
 
-if True:
+
+
+if r"C:\Users" in BASE_DIR:
+    switch=False
+else:
+    switch=True
+
+SECURE_SSL_REDIRECT=switch
+
+if switch:
     # aws settings
-    
+    AWS_ACCESS_KEY_ID = 'AKIAJSDAPWVWN4OZZHQA'
+    AWS_SECRET_ACCESS_KEY = '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'
+    AWS_STORAGE_BUCKET_NAME = 'carenmarketplace'
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com'%(AWS_STORAGE_BUCKET_NAME)
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    AWS_LOCATION = 'static'
+    STATIC_URL = 'https://%s/%s/'%(AWS_S3_CUSTOM_DOMAIN,AWS_LOCATION)
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    # DATABASES = {
+    # 'default': {
+    # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    # 'NAME': 'dc7th8ploivfp5',
+    # 'USER': 'tjxvxajkvetvno',
+    # 'PASSWORD': 'c11c9ae1d2491b804c4fed88542dfee1baef0ca881ab8d3fcef8418c4879bc4a',
+    # 'HOST': 'ec2-174-129-27-158.compute-1.amazonaws.com',
+    # 'PORT': '5432',}}
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -114,36 +141,26 @@ if True:
 }
 
 else:
-    AWS_ACCESS_KEY_ID = 'AKIAJ5AJT5MYJXNDL7XQ'
-    AWS_SECRET_ACCESS_KEY = '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'
-    AWS_STORAGE_BUCKET_NAME = 'carenrentacar'
-    AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com'%(AWS_STORAGE_BUCKET_NAME)
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-    # s3 static settings
-    AWS_LOCATION = 'static'
-    STATIC_URL = 'https://%s/%s/'%(AWS_S3_CUSTOM_DOMAIN,AWS_LOCATION)
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATIC_URL = '/static/'
+    STATIC_ROOT = BASE_DIR + '\static'
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),}}
-        
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR + r'\static'
+
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'youremail@gmail.com'
-EMAIL_HOST_PASSWORD = 'yourpassword'
+EMAIL_HOST = 'smtp.carly.do'
+EMAIL_HOST_USER = 'admin@carly.do'
+EMAIL_HOST_PASSWORD = 'xlzl!Mo5'
 EMAIL_PORT = 587
 
-CORS_ORIGIN_WHITE_LIST = '127.0.0.1','127.0.0.1:3000',
+CORS_ORIGIN_WHITE_LIST = '127.0.0.1','localhost','carenproject.herokuapp.com','3.15.162.59',
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_ALLOW_ALL = True
 JWT_AUTH = {
     'JWT_RESPONSE_PAYLOAD_HANDLER' :   'kamailio.utils.custom_jwt_response_handler',
     'JWT_EXPIRATION_DELTA': datetime.timedelta(minutes=999999999)
 }
-
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
+
+
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT;
+const app = express();
+const history = require('connect-history-api-fallback')
+const https = require('https');
+const http = require('http');
+const compression = require('compression');
+const server = http.createServer(/** … */);
+const GracefulShutdownManager = require('@moebius/http-graceful-shutdown').GracefulShutdownManager;
+const fs = require('mz/fs');
+const httpShutdownManager = new GracefulShutdownManager(server);
+
+process.on('SIGTERM', () => {
+	httpsShutdownManager.terminate(() => {
+		httpShutdownManager.terminate(() => {
+			console.log('Server is gracefully terminated');
+		});
+	});
+});
+app.use(compression());
+
+async function start(){
+  app.use(history({
+    verbose: true
+  }))
+  
+  async function getCerts(){
+    const certdir = (await fs.readdir("/etc/letsencrypt/live"))[1];
+    return {
+      key: await fs.readFile(`/etc/letsencrypt/live/${certdir}/privkey.pem`),
+      cert: await fs.readFile(`/etc/letsencrypt/live/${certdir}/fullchain.pem`)
+    }
+  }
+  
+  const {key, cert} = await getCerts()
+  
+  // the __dirname is the current directory from where the script is running
+  app.use(express.static(path.join(__dirname, './build'), { maxAge: 30 * 60 * 60 * 24 * 1000 }));
+  // send the user to index html page inspite of the url
+  app.get('*', (req, res) => {
+    res.sendfile(path.resolve(__dirname, './build/index.html'));
+  });
+  
+const httpApp = express();
+httpApp.get('*', function(req, res) {  
+  res.redirect('https://' + req.headers.host + req.url);
+  console.log(req.headers.host + req.url,'redirecting')
+})
+const httpServer = http.createServer(httpApp).listen(80);
+https.createServer({key, cert}, app).listen(443)
+}
+
+start()
