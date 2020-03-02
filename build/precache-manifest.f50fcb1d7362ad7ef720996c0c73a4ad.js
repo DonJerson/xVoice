a99c25b704436e8903227ab33a1f8031fb69de88
prieto <prieto@desktop.com>
2020-03-01 20:08:27 -0400

    upload

---
+++ build/precache-manifest.f50fcb1d7362ad7ef720996c0c73a4ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8fc2d5bc32058ccf5085ed60ad4cde0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70c340607de47fadb0df",
+    "url": "/static/css/main.f5f8e4e9.chunk.css"
+  },
+  {
+    "revision": "0f4f9901a3ce56e0dca0",
+    "url": "/static/js/2.533ed2d8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.533ed2d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c340607de47fadb0df",
+    "url": "/static/js/main.45dd118d.chunk.js"
+  },
+  {
+    "revision": "2ac2632f7f4f6958f3d8",
+    "url": "/static/js/runtime-main.1d6cc992.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
