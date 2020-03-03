0aa02ac2b8e2efdce6303189c00ddf7e978ad560
prieto <prieto@desktop.com>
2020-03-02 23:03:46 -0400

    autoUpdate

--- worker.py
+++ worker.py
@@ -13,28 +13,31 @@ while True:
             print("found")
             try:
                 consumer = Subscriber.objects.get(username=logs[index].src_user).customer
-            except Exception as e:
-                print("errorcito")
-                print(e)
-            startDate=""
-            endDate=""
+                startDate=""
+                endDate=""
 
-            for log in newLogs:
-                log.consumer=consumer
-                log.save()
-                if log.method =="INVITE":
-                    startDate=log.time
-                else: endDate=log.time
-            diff = (endDate-startDate).seconds/60
-            destination = logs[index].dst_user
-            if destination[0]=="1":
-                rate=0.010
-            try:
-                consumer.balance=float(consumer.balance)-rate*diff
+                for log in newLogs:
+                    log.consumer=consumer
+                    log.save()
+                    if log.method =="INVITE":
+                        startDate=log.time
+                    else: endDate=log.time
+                diff = (endDate-startDate).seconds/60
+                destination = logs[index].dst_user
+                if destination[0]=="1":
+                    rate=0.010
+                try:
+                    consumer.balance=float(consumer.balance)-rate*diff
+                except Exception as e:
+                    consumer.balance=-rate*diff
+                consumer.save()
+                logs = logs.exclude(callid=logs[index].callid)
             except Exception as e:
-                consumer.balance=-rate*diff
-            consumer.save()
-            logs = logs.exclude(callid=logs[index].callid)
+                print("errorcito fetching")
+                print(logs[index].src_user).customer)
+                print(e)
+                
+
         else:
             print("not found")
             index+=1
