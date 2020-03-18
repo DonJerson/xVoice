d06ded8c57ff45fedca9e3d4495d8eb96e27b342
prieto <prieto@desktop.com>
2020-03-18 16:50:49 -0400

    autoUpdate

--- all.sh
+++ all.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
-rtpproxy -l 172.31.29.81 -s udp:127.0.0.1:7722 -u rtpproxy:rtpproxy
 screen -d -m -RR django -t django bash -c 'cd /home/ubuntu && source personalvenv/bin/activate && cd /home/ubuntu/xvoice && git stash && git pull origin master && gunicorn -b 0.0.0.0:8181 finalminutes.wsgi:application --certfile /etc/letsencrypt/live/xvoice.xyz/fullchain.pem --keyfile /etc/letsencrypt/live/xvoice.xyz/privkey.pem'
 screen -d -m -RR node -t node bash -c 'cd /home/ubuntu/xvoice && node server.js'
 screen -d -m -RR worker -t worker bash -c 'cd /home/ubuntu && source personalvenv/bin/activate && cd /home/ubuntu/xvoice && python manage.py shell'
