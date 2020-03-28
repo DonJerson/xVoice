a17bd7ddf681ecf3f931795d164cbe288381dbdb
Your Name <you@example.com>
2020-03-28 15:04:10 -0400

    autoUpdate

--- worker.py
+++ worker.py
@@ -1,47 +1,69 @@
 from kamailio.models import Acc,Subscriber
 import time
+import threading
 
-while True:
+class Worker():
+    def __init__(self):
+        self.mainLock = threading.Lock()
+        self.logStart = Acc.objects.filter(consumer__isnull=True,method="INVITE")
+        print("len")
+        print(len(self.logStart))
+        self.logEnd = Acc.objects.filter(consumer__isnull=True,method="INVITE")
+        self.initAll()
+        return
 
-    logs = Acc.objects.filter(consumer__isnull=True)
-    print("len")
-    print(len(logs))
-    #logs = Acc.objects.all()
-    index = 0
-    while index < len(logs):
-        newLogs = logs.filter(callid=logs[index].callid)
-        print(newLogs)
-        if len(newLogs)==2:
+    def fetchLog(self):
+        mainLock.acquire()
+        newLog = self.logStart[0]
+        self.logStart=[1:]
+        mainLock.release()
+        return newLog
+
+    def fetchEndLog(self,id):
+        logEnd = self.logEnd.filter(callid=newLog.callid)
+        self.logEnd = self.logEnd.exclude(callid=logs[index].callid)
+        return logEnd
+
+    def recordData(self):
+        newLog = self.fetchLog()
+        try:
+            logEnd = self.fetchEndLog()
+            time.sleep(1)
+            consumer = Subscriber.objects.get(username=newLog.src_user).customer
             print("found")
+            startDate=newLog.time
+            endDate=logEnd.time
+            newLog.consumer=consumer
+            newLog.save()
+            logEnd.consumer=consumer
+            logEnd.save()
+            diff = (endDate-startDate).seconds/60
+            destination = logs[index].dst_user
+            if destination[0]=="1":
+                rate=0.010
             try:
-                consumer = Subscriber.objects.get(username=logs[index].src_user).customer
-                startDate=""
-                endDate=""
-
-                for log in newLogs:
-                    log.consumer=consumer
-                    log.save()
-                    if log.method =="INVITE":
-                        startDate=log.time
-                    else: endDate=log.time
-                diff = (endDate-startDate).seconds/60
-                destination = logs[index].dst_user
-                if destination[0]=="1":
-                    rate=0.010
-                try:
-                    consumer.balance=float(consumer.balance)-rate*diff
-                except Exception as e:
-                    consumer.balance=-rate*diff
-                consumer.save()
-                logs = logs.exclude(callid=logs[index].callid)
+                consumer.balance=float(consumer.balance)-rate*diff
             except Exception as e:
-                print("errorcito fetching")
-                print(logs[index].src_user)
-                print(logs[index].id)
-                print(e)
+                consumer.balance=-rate*diff
+            consumer.save()
+        except:
+            print("errorcito fetching")
+            print("newLog")
                 
+    return
 
-        else:
-            print("not found")
-            index+=1
-    time.sleep(5)
+    def initAll(self):
+        while len(self.startLog)>0:
+            for y in range(100):
+                threads = []
+                t = threading.Thread(target=self.recordData)
+                t.start()
+                threads.append(t)
+                
+            for thread in threads:
+                t.join()
+                pass
+ 
+            time.sleep(5)
+
+worker = Worker()
