9d97f5c6dca3843a346df03c904f3caea338050a
Your Name <you@example.com>
2020-04-15 00:52:14 -0400

    autoUpdate

---
+++ src/Dialer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Dialer extends Component {
+    state = {  }
+    render() { 
+        return ( <>
+        
+        
+        
+        </> );
+    }
+}
+ 
+export default Dialer;
