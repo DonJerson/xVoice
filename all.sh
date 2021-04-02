84deafa7930980e6ab443e1e5d5b0ee90fe32f79
EC2 Default User <ec2-user@ip-172-31-17-2.us-east-2.compute.internal>
2021-04-02 18:33:42 +0000

    movie

--- all.sh
+++ all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
-screen -d -m -RR django -t django bash -c 'cd /home/ubuntu && source personalvenv/bin/activate && cd /home/ubuntu/xvoice && git stash && git pull origin master && gunicorn -b 0.0.0.0:8181 finalminutes.wsgi:application --certfile /etc/letsencrypt/live/xvoice.xyz/fullchain.pem --keyfile /etc/letsencrypt/live/xvoice.xyz/privkey.pem'
+screen -d -m -RR django -t django bash -c 'cd /home/ubuntu && source xvoicevenv/bin/activate && cd /home/ubuntu/xvoice && git stash && git pull origin master && gunicorn -b 0.0.0.0:8181 finalminutes.wsgi:application --certfile /etc/letsencrypt/live/xvoice.xyz/fullchain.pem --keyfile /etc/letsencrypt/live/xvoice.xyz/privkey.pem'
 screen -d -m -RR node -t node bash -c 'cd /home/ubuntu/xvoice && node server.js'
-screen -d -m -RR worker -t worker bash -c 'cd /home/ubuntu && source personalvenv/bin/activate && cd /home/ubuntu/xvoice && python manage.py shell'
+#screen -d -m -RR worker -t worker bash -c 'cd /home/ubuntu && source xvoicevenv/bin/activate && cd /home/ubuntu/xvoice && python manage.py shell'
--- finalminutes/settings.py
+++ finalminutes/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'c06bxt!5&^yk8h&3pt!qeki69+_42v0j^fnydk1b*4wd6l@p7l'
     
 DEBUG = True
 CORS_ORIGIN_ALLOW_ALL = True
-ALLOWED_HOSTS = ['3.135.1.254','xvoice.xyz','192.168.2.109','192.168.1.100','127.0.0.1','www.xvoice.xyz','http://xvoice.xyz','https://xvoice.xyz']
+ALLOWED_HOSTS = ['3.137.193.102','xvoice.xyz','192.168.2.109','192.168.1.100','127.0.0.1','www.xvoice.xyz','http://xvoice.xyz','https://xvoice.xyz']
 
 INSTALLED_APPS = [
     "sslserver",
@@ -135,7 +135,7 @@ if True:
         'NAME': 'kamailio',
         'USER': 'root',
         'PASSWORD': 'Pri3to.Server',
-        'HOST': '3.135.1.254',
+        'HOST': '3.137.193.102',
         'PORT': '3306',
     }
 }
@@ -154,7 +154,7 @@ EMAIL_HOST_USER = 'admin@carly.do'
 EMAIL_HOST_PASSWORD = 'xlzl!Mo5'
 EMAIL_PORT = 587
 
-CORS_ORIGIN_WHITE_LIST = '3.135.1.254','127.0.0.1','localhost','carenproject.herokuapp.com','3.15.162.59','http://xvoice.xyz','www.xvoice.xyz','https://xvoice.xyz','https://www.xvoice.xyz',
+CORS_ORIGIN_WHITE_LIST = '3.137.193.102','127.0.0.1','localhost','carenproject.herokuapp.com','3.15.162.59','http://xvoice.xyz','www.xvoice.xyz','https://xvoice.xyz','https://www.xvoice.xyz',
 CORS_ALLOW_CREDENTIALS = True
 JWT_AUTH = {
     'JWT_RESPONSE_PAYLOAD_HANDLER' :   'kamailio.utils.custom_jwt_response_handler',
