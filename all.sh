48175daf1181347dd29b1e1a606cfbecd7818e7d
EC2 Default User <ec2-user@ip-172-31-17-2.us-east-2.compute.internal>
2021-04-02 18:44:41 +0000

    up

--- all.sh
+++ all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
-screen -d -m -RR django -t django bash -c 'cd /home/ubuntu && source xvoicevenv/bin/activate && cd /home/ubuntu/xvoice && git stash && git pull origin master && gunicorn -b 0.0.0.0:8181 finalminutes.wsgi:application --certfile /etc/letsencrypt/live/xvoice.xyz/fullchain.pem --keyfile /etc/letsencrypt/live/xvoice.xyz/privkey.pem'
-screen -d -m -RR node -t node bash -c 'cd /home/ubuntu/xvoice && node server.js'
-#screen -d -m -RR worker -t worker bash -c 'cd /home/ubuntu && source xvoicevenv/bin/activate && cd /home/ubuntu/xvoice && python manage.py shell'
+screen -d -m -RR django -t django bash -c 'cd /usr/local/lib/xvoice && source xvoicevenv/bin/activate && cd /usr/local/lib/xvoice && git stash && git pull origin master && gunicorn -b 0.0.0.0:8181 finalminutes.wsgi:application'
+#screen -d -m -RR node -t node bash -c 'cd /usr/local/lib/xvoice && node server.js'
+#screen -d -m -RR worker -t worker bash -c 'cd /usr/local/lib && source xvoicevenv/bin/activate && cd /usr/local/lib/xvoice && python manage.py shell'
