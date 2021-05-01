b5515d20ef40580c9b6f68000c4f09495ad4e3cb
System Administrator <root@Jersons-MacBook-Pro.local>
2021-05-01 13:59:15 -0400

    niceUp

---
+++ build/precache-manifest.6ea0b5146b071a3365dfc33434e0fd95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4461450499019c9f9da6617ededdd4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a0704a22cd0bd88a8d5",
+    "url": "/static/css/main.73accb8e.chunk.css"
+  },
+  {
+    "revision": "79e1164369c67c226d52",
+    "url": "/static/js/2.45f6126b.chunk.js"
+  },
+  {
+    "revision": "3fef434c705e194cec4b01b323feb7b5",
+    "url": "/static/js/2.45f6126b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a0704a22cd0bd88a8d5",
+    "url": "/static/js/main.090529d5.chunk.js"
+  },
+  {
+    "revision": "d8e7b386f478c4b7a853",
+    "url": "/static/js/runtime-main.fb2e52dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
